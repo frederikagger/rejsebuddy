3cf0f15d7a318dd8db43b18c58bf2a6ee6ef13ba
frederikagger <frederikagger@hotmail.com>
2021-04-21 21:54:21 +0200

    wrapped app in userProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import './../styles/tailwind.css'
+import Header from '../components/Header'
+import { UserProvider } from '../components/UserContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <div className='bg-primary min-h-screen'>
+        <Header />
+        <div className='mt-20'>
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </UserProvider>
+  )
 }
 
 export default MyApp
