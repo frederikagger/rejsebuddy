601eeb722a7590985c9ffee5dd6ce3943c7ffdf4
frederikagger <frederikagger@hotmail.com>
2021-04-25 14:00:58 +0200

    added more margin

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ const LoadingSpinner: React.FC = () => {
   return (
     <div className='container mx-auto'>
       <img
-        className='animate-spin mt-10 h-10 w-10 mx-auto'
+        className='animate-spin mt-48 h-10 w-10 mx-auto'
         src='/images/loader.svg'
         alt='loader'
       />
