0846a19334f8f5871b8b82b4382f5b0427f1b9a3
Frederik Agger <48592250+frederikagger@users.noreply.github.com>
2021-06-16 16:58:57 +0200

    renamed env variables (#26)

--- utils/AWS-S3.ts
+++ utils/AWS-S3.ts
@@ -2,9 +2,9 @@ import S3 from 'aws-sdk/clients/s3';
 
 const S3client = new S3({
     apiVersion: "2006-03-01",
-    region: process.env.AWS_REGION,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    region: process.env.REGION,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_KEY_ID,
 });
 
 const uploadS3: (image: Buffer, userId: string) => Promise<string> = async (image, userId) => {
