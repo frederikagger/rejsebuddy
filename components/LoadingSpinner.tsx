a780edc33f3b84c9ff06189582d600f7ac9cb8c2
frederikagger <frederikagger@hotmail.com>
2021-06-20 15:26:44 +0200

    fixing deletetion of user

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -1,8 +1,8 @@
 const LoadingSpinner: React.FC = () => {
   return (
-    <div className='container mx-auto'>
+    <div className='absolute left-1/2 top-1/2 transform-gpu -translate-x-1/2 -translate-y-1/2'>
       <img
-        className='animate-spin mt-48 h-10 w-10 mx-auto'
+        className='animate-spin h-10 w-10'
         src='/images/loader.svg'
         alt='loader'
       />
